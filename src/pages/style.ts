94b0dcd20e25077f374d6973f19572bf8f46f2ad
Junior <reinaldo.diasjunior@gmail.com>
2021-01-26 22:59:07 -0300

    Adds style to SignIn page

--- src/pages/style.ts
+++ src/pages/style.ts
@@ -16,6 +16,40 @@ export const Content = styled.div`
 
   width: 100%;
   max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 24px;
+  }
+
+  input {
+    background: #232129;
+    border-radius: 12px;
+    border: 2px solid #232129;
+    padding: 16px;
+    width: 100%;
+
+    & + input {
+      margin-top: 8px;
+    }
+  }
+
+  button {
+    background: #ff9000;
+    border-radius: 12px;
+    height: 56px;
+    color: #312e38;
+    border: 0;
+    padding: 0 16px;
+    margin-top: 16px;
+    width: 100%;
+    font-weight: 500;
+  }
 `;
 
 export const Background = styled.div`
