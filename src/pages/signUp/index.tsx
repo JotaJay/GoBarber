c219ede8c58bed4de12800b9f998935afc9d6b2b
Junior <reinaldo.diasjunior@gmail.com>
2021-01-28 14:42:22 -0300

    Adds unform lib

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
-import Input from '../../components/input';
-import Button from '../../components/button';
-import { Container, Content, Background } from './style';
+import { Form } from '@unform/web';
+
 import logo from '../../assets/logo.svg';
 
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+import { Container, Content, Background } from './style';
+
 const SignUp: React.FC = () => {
+  const handleSubmit = (data: Record<string, unknown>): void => {
+    console.log(data);
+  };
+
   return (
     <>
       <Container>
         <Background />
         <Content>
           <img src={logo} alt="GoBarber" />
-          <form>
+          <Form onSubmit={handleSubmit}>
             <h1>Sign Up</h1>
             <Input name="name" placeholder="Name" icon={FiUser} />
             <Input name="email" placeholder="E-mail" icon={FiMail} />
@@ -23,7 +31,7 @@ const SignUp: React.FC = () => {
               icon={FiLock}
             />
             <Button type="submit">Sign Up</Button>
-          </form>
+          </Form>
           <a href="/">
             <FiArrowLeft /> Back to Sign In
           </a>
